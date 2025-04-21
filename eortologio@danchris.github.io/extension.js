ee4d078409909761321d3464640403f759dc2302
Nathaniel Russell <46272571+nater1983@users.noreply.github.com>
2025-04-20 20:00:42 -0500

    Update extension.js
    Add in tomorrow's namedays as well as give them a drop down menu called Tomorrow

--- eortologio@danchris.github.io/extension.js
+++ eortologio@danchris.github.io/extension.js
@@ -21,7 +21,7 @@ import GLib from 'gi://GLib';
 import St from 'gi://St';
 import Clutter from 'gi://Clutter';
 
-import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
+import { Extension, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 import * as Helpers from './helpers.js';
@@ -30,77 +30,83 @@ import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 
 const EortologioPopup = GObject.registerClass(
-  class EortologioPopup extends PanelMenu.Button {
+class EortologioPopup extends PanelMenu.Button {
     _init() {
-      super._init(0);
-      let label = new St.Label({
-        text: "Eortologio",
-        y_align: Clutter.ActorAlign.CENTER,
-      });
-
-      this.add_child(label);
-     
-      // set the label that describes the button for screenreader users
-      this.set_label_actor(label);
- 
-      // This is the time at construction, which is the same as the time
-      // when `enable()` is called.
-      this.dateTime = GLib.DateTime.new_now_local();
-      let initialNamedays = Helpers.getNameDays(this.dateTime);
-      updateMenu(this.menu, initialNamedays);
-      
-      this._timeoutId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT,
-        60 * 60, () => {
- 
-          // This is current time when the callback is invoked we are now inside
-          let currDateTime = GLib.DateTime.new_now_local();
-          if (this.dateTime.get_day_of_month() != currDateTime.get_day_of_month() ||
-            this.dateTime.get_month() != currDateTime.get_month() ||
-            this.dateTime.get_year() != currDateTime.get_year()
-          ) {
-            // The day has changed, so we update the reference time and menu
-            this.dateTime = currDateTime;
-            let currentNamedays = Helpers.getNameDays(this.dateTime);
-            updateMenu(this.menu, currentNamedays);
-          }
-          return GLib.SOURCE_CONTINUE;
+        super._init(0);
+        let label = new St.Label({
+            text: _("Eortologio"),
+            y_align: Clutter.ActorAlign.CENTER,
         });
-        
-      this.connect('destroy', () => {
-        GLib.Source.remove(this._timeoutId);
-        this._timeoutId = null;
-      });
 
+        this.add_child(label);
+
+        // Set the label for screen readers
+        this.set_label_actor(label);
+
+        // Initial time and nameday population
+        this.dateTime = GLib.DateTime.new_now_local();
+        let todayNames = Helpers.getNameDays(this.dateTime);
+        let tomorrowNames = Helpers.getNameDays(this.dateTime.add_days(1));
+        updateMenu(this.menu, todayNames, tomorrowNames);
+
+        // Update every hour
+        this._timeoutId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 60 * 60, () => {
+            let currDateTime = GLib.DateTime.new_now_local();
+            if (this.dateTime.get_day_of_month() !== currDateTime.get_day_of_month() ||
+                this.dateTime.get_month() !== currDateTime.get_month() ||
+                this.dateTime.get_year() !== currDateTime.get_year()) {
+                
+                this.dateTime = currDateTime;
+                let todayNames = Helpers.getNameDays(this.dateTime);
+                let tomorrowNames = Helpers.getNameDays(this.dateTime.add_days(1));
+                updateMenu(this.menu, todayNames, tomorrowNames);
+            }
+            return GLib.SOURCE_CONTINUE;
+        });
+
+        this.connect('destroy', () => {
+            GLib.Source.remove(this._timeoutId);
+            this._timeoutId = null;
+        });
     }
-  }
-);
+});
 
 export default class EortologioPopupExtension extends Extension {
-  enable() {
-    this._EortologioPopup = new EortologioPopup();
-    Main.panel.addToStatusArea(this.uuid, this._EortologioPopup);
-  }
+    enable() {
+        this._EortologioPopup = new EortologioPopup();
+        Main.panel.addToStatusArea(this.uuid, this._EortologioPopup);
+    }
 
-  disable() {
-    this._EortologioPopup.destroy();
-    this._EortologioPopup = null;
-  }
+    disable() {
+        this._EortologioPopup.destroy();
+        this._EortologioPopup = null;
+    }
 }
 
-function updateMenu(menu, names) {
-
-  if (!menu.isEmpty()) {
-    menu.removeAll()
-  }
-
-  if (names.length === 0){
-    menu.addMenuItem(new PopupMenu.PopupMenuItem('No Celebrations today...'));
-  }
-  else {
-    let popupMenuExpander = new PopupMenu.PopupSubMenuMenuItem('Today');
-    for (let i = 0; i < names.length; i++){
-      popupMenuExpander.menu.addMenuItem(new PopupMenu.PopupMenuItem(names[i]));
+function updateMenu(menu, todayNames, tomorrowNames) {
+    if (!menu.isEmpty()) {
+        menu.removeAll();
     }
-    menu.addMenuItem(popupMenuExpander);
-  }
+
+    // Today section
+    if (todayNames.length === 0) {
+        menu.addMenuItem(new PopupMenu.PopupMenuItem(_('No Celebrations today...')));
+    } else {
+        let todaySubmenu = new PopupMenu.PopupSubMenuMenuItem(_('Today'));
+        for (let name of todayNames) {
+            todaySubmenu.menu.addMenuItem(new PopupMenu.PopupMenuItem(name));
+        }
+        menu.addMenuItem(todaySubmenu);
+    }
+
+    // Tomorrow section
+    if (tomorrowNames.length === 0) {
+        menu.addMenuItem(new PopupMenu.PopupMenuItem(_('No Celebrations tomorrow...')));
+    } else {
+        let tomorrowSubmenu = new PopupMenu.PopupSubMenuMenuItem(_('Tomorrow'));
+        for (let name of tomorrowNames) {
+            tomorrowSubmenu.menu.addMenuItem(new PopupMenu.PopupMenuItem(name));
+        }
+        menu.addMenuItem(tomorrowSubmenu);
+    }
 }
